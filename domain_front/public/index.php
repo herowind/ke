ce144de93508f876a354af18cb0a771c778f1f00
Oliver Wang <whtaok@163.com>
2017-11-24 00:28:34 +0800

    修改入口文件

--- domain_front/public/index.php
+++ domain_front/public/index.php
@@ -1,6 +1,6 @@
 <?php
 namespace think;
-require 'site_local.php';
+require 'site_prod.php';
 require __DIR__ . '/../../easywechat/vendor/autoload.php';
 require __DIR__ . '/../../framework/base.php';
 Container::get('app')->run()->send();
