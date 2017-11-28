4a9396cc14928f17390d9acac42ece4201fb7648
Oliver Wang <whtaok@163.com>
2017-11-29 00:05:39 +0800

    jump

---
+++ domain_front/public/exception.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+<title>系统发生错误</title>
+<script type="text/javascript" src="/lib/utils/jquery.min-2.2.1.js"></script>
+<script type="text/javascript" src="/lib/components/layer/layer.js"></script>
+</head>
+
+<body>
+<script type="text/javascript">
+$(function() {
+	layer.alert("未预期的错误发生，请重新尝试或联系管理员", {
+		icon: 2,
+		title: "系统发生错误"
+	})
+})
+</script>
+</body>
+
+</html>
+
--- domain_front/public/jump.php
+++ domain_front/public/jump.php
@@ -2,38 +2,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
-    <title>跳转提示</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+<title>跳转提示</title>
 <script type="text/javascript" src="/lib/utils/jquery.min-2.2.1.js"></script>
 <script type="text/javascript" src="/lib/components/layer/layer.js"></script>
 </head>
 
 <body>
-<b id="wait" style='display: none'> <?php echo($wait); ?></b>
 <a id="href" href="<?php echo($url); ?>" style='display: none'>跳转</a>
 <script type="text/javascript">
 $(function() {
-	var code 	= "<?php echo($code); ?>";
-	var msg 	= "<?php echo(strip_tags($msg)); ?>";
-	var url 	= "<?php echo($url); ?>";
-	var wait 	= "<?php echo($wait); ?>";
-	var title   = "页面自动跳转中... 等待时间:" + wait + "秒";
-	var icon    = 1;
+	var title   = "页面自动跳转中... 等待时间:<b id='wait'><?php echo($wait); ?></b>秒";
 	<?php switch ($code) {?>
 		<?php case 1:?>
-			icon = 1;
+			var icon = 1;
 		<?php break;?>
         <?php case 0:?>
-        	icon = 2;
+        	var icon = 2;
         <?php break;?>
 	<?php } ?>
-	layer.alert(msg, {
+	layer.alert("<?php echo(strip_tags($msg)); ?>", {
 		icon: icon,
 		title: title,
 		success: function(layer) {
-			var wait = document.getElementById('wait'),
-				href = document.getElementById('href').href;
+			var wait = document.getElementById('wait');
+			var	href = document.getElementById('href').href;
 			var interval = setInterval(function() {
 				var time = --wait.innerHTML;
 				if (time <= 0) {
@@ -41,8 +35,10 @@ $(function() {
                     clearInterval(interval);
                 };
             }, 1000);
-       });
-	</script>
+       }
+	})
+})
+</script>
 </body>
 
 </html>
